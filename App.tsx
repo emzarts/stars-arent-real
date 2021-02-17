ea7884b82b9e8d0fbea3d8dfd64ff91af11076f4
Devin Matte <devinmatte@gmail.com>
2021-02-17 09:46:01 -0500

    Adding basic constelation code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ConstellationSketcher from 'react-constellation-sketcher';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <ConstellationSketcher width="500"
+                                  height="500"
+                                  slideshow={true}
+            />
     </View>
   );
 }
