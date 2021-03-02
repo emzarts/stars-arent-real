c03dd3f79044448cee06509ad92a76edfaa7ce01
Emily Zimmerman <17zimmermane@gmail.com>
2021-03-01 20:36:29 -0500

    star map added to home page for fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import "./starmap.ts";
 
 // @ts-ignore
 import ConstellationSketcher from "react-constellation-sketcher";
 import React from "react";
 import logo from "./logo.svg";
+import StarMap from "./StarMap";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <StarMap></StarMap>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/StarMap.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useEffect } from "react";
+
+const StarMap = () => {
+  useEffect(() => {
+    // add the script tag to the HTML after component is rendered
+    // otherwise the canvas with id sky is not found and the image won't show up
+    if (document.getElementById("skySphereScript") === null) {
+      const skySphere = document.createElement("script");
+      skySphere.setAttribute("id", "skySphereScript");
+      skySphere.src = "../public/sky-sphere.min.js";
+      skySphere.async = true;
+      skySphere.setAttribute("type", "text/html");
+      document.body.appendChild(skySphere);
+      const renderSkySphere = document.createElement("script");
+      renderSkySphere.innerHTML =
+        " const sky = new SkySphere('sky', { " +
+        "width: 800," +
+        "height: 400," +
+        "customOnClick: function (data) {" +
+        "console.log('REEE', data);" +
+        "}," +
+        "});" +
+        "sky.drawSky();";
+
+      document.body.appendChild(skySphere);
+      document.body.appendChild(renderSkySphere);
+    }
+  }, []);
+  return (
+    <div className="StarMap">
+      <canvas id="sky"></canvas>
+    </div>
+  );
+};
+
+export default StarMap;
