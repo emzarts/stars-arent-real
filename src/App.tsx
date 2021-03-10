ac36578dcd197b1efb28072443506a1e39e469fc
Emily Zimmerman <17zimmermane@gmail.com>
2021-03-10 09:22:23 -0500

    adding the starMap page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import "./App.css";
-
 import { Route, Router, Switch } from "react-router-dom";
-
+import { createBrowserHistory } from "history";
 // @ts-ignore
 import ConstellationSketcher from "react-constellation-sketcher";
+import LearnStarMap from "./pages/LearnStarMap";
 import React from "react";
-import { createBrowserHistory } from "history";
-import StarMap from "./StarMap";
-import logo from "./logo.svg";
 
 const history = createBrowserHistory();
 
 const App: React.FC = () => {
-  const learnReactString = "Learn React";
-
   return (
     <Router history={history}>
       <Switch>
@@ -23,27 +18,19 @@ const App: React.FC = () => {
           component={() => (
             <div className="App">
               <header className="App-header">
-                <StarMap></StarMap>
-                <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-                </p>
+                <h1>Welcome to Stars Aren't Real</h1>
                 <ConstellationSketcher
                   width="500"
                   height="500"
                   slideshow={true}
                 />
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {learnReactString}
-                </a>
               </header>
             </div>
           )}
         />
+        <Route path="/learn-map">
+          <LearnStarMap />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/pages/LearnStarMap.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StarMap from "../StarMap";
+
+const LearnStarMap = () => {
+  return (
+    <div className="learStarMapPage page">
+      <StarMap />
+    </div>
+  );
+};
+
+export default LearnStarMap;
