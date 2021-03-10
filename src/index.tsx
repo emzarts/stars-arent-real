c21aa97725560b0d9a8fbd90e450e78497a6a52d
meg5228 <meg5228@g.rit.edu>
2021-03-10 09:15:41 -0500

    Creation of constellation data, constellation list component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import App from "./App";
+import ConstellationList from "./constellation-list";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
