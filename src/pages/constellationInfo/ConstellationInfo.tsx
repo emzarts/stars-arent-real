b6b4ee38e6a00f8b13549484c04163f514e020b9
Emily Zimmerman <17zimmermane@gmail.com>
2021-03-17 20:04:42 -0400

    fixing alphabetical imports

--- src/pages/constellationInfo/ConstellationInfo.tsx
+++ src/pages/constellationInfo/ConstellationInfo.tsx
@@ -1,8 +1,7 @@
 import "./ConstellationInfo.css";
-
+import ConstellationInfoContainer from "./constellationInfoContainer/ConstellationInfoContainer";
 import React from "react";
 import { useParams } from "react-router-dom";
-import ConstellationInfoContainer from "./constellationInfoContainer/ConstellationInfoContainer";
 
 const ConstellationInfo: React.FC = () => {
   const { name } = useParams<Record<string, string>>();
