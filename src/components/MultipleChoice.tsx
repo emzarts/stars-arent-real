4ef189032c5cf3a787dbacb6eecefd3c4b4e998e
Emily Zimmerman <17zimmermane@gmail.com>
2021-03-18 19:49:36 -0400

    handled the async call to get the quiz questions

--- src/components/MultipleChoice.tsx
+++ src/components/MultipleChoice.tsx
@@ -1,9 +1,22 @@
+import React, { useEffect, useState } from "react";
 import Quiz from "react-quiz-component";
 import { createQuiz } from "../QuizSource";
-import React from "react";
 
 const MultipleChoice: React.FC = () => {
-  return <Quiz quiz={createQuiz()} />;
+  const [quizGenerated, setQuizGenerated] = useState(false);
+  const [questions, setQuestions] = useState({});
+  useEffect(() => {
+    createQuiz().then((res) => {
+      setQuestions(res);
+      setQuizGenerated(true);
+      console.log("created");
+    });
+  }, []);
+  return (
+    <div className="">
+      {quizGenerated ? <Quiz quiz={questions} /> : <div></div>}
+    </div>
+  );
 };
 
 export default MultipleChoice;
